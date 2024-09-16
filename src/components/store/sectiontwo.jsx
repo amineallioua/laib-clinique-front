2c7292b412c679f16b759e137ce3894f911906a0
amineallioua <amineallioua123@gmail.com>
2024-09-16 18:26:43 +0100

    responsive

--- src/components/store/sectiontwo.jsx
+++ src/components/store/sectiontwo.jsx
@@ -6,14 +6,15 @@ const Ssec2 = () => {
     return ( 
         <section className=" w-screen  pt-[70px] px-12 " >
             <h1 className="text-center mb-10 text-2xl sm:text-4xl font-extrabold  text-black" > A wide range of items </h1>
-            <div className='  grid grid-cols-3 gap-3 mb-10' > 
+            <center>
+            <div className='  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mb-10' > 
             <Leftcard   /> 
-            <Leftcard  className=" h-[410px] w-[250px] row-span-2 " /> 
+            <Leftcard  className=" md:h-[410px] w-[250px] row-span-2 " /> 
             <Leftcard /> 
             <Leftcard  /> 
             <Leftcard /> 
             </div>
-            <div className='grid grid-cols-5 gap-5 mb-10 ' >
+            <div className='grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2   gap-5 mb-10 ' >
                 <Itemcard/>
                 <Itemcard/>
                 <Itemcard/>
@@ -28,7 +29,7 @@ const Ssec2 = () => {
                 <Itemcard/>
                 <Itemcard/>
             </div>
-
+            </center>
 
         </section>
      );
